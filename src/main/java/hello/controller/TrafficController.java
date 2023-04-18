2642548f3581c7641a65847cbbeaddb16d22d819
HangkwonCho <vatican0411@gmail.com>
2023-04-19 00:19:02 +0900

    그라파다

--- src/main/java/hello/controller/TrafficController.java
+++ src/main/java/hello/controller/TrafficController.java
@@ -1,9 +1,13 @@
 package hello.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,5 +34,23 @@ public class TrafficController {
             list.add("hello jvm!" + i);
         }
         return "ok";
+    }
+
+    @Autowired
+    DataSource dataSource;
+
+    @GetMapping("/jdbc")
+    public String jdbc() throws SQLException {
+        log.info("jdbc");
+        Connection conn = dataSource.getConnection();
+        log.info("connection info={}", conn);
+        //conn.close();
+        return "ok";
+    }
+
+    @GetMapping("/error-log")
+    public String errorLog() {
+        log.error("error log");
+        return "error";
     }
 }
